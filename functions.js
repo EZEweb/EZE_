8f38bf7691583cce5d6ce638275bc53b876015ad
Ezequiel Rico <ezequielrico@outlook.com>
2022-03-31 20:31:12 -0300

    prboando menu hamburguesa

--- functions.js
+++ functions.js
@@ -2,4 +2,7 @@ $( document ).ready(function() {
     $('#menu_on').click(function(){
     	$('body').toggleClass('visible_menu');
     })
+    $('#menu_off').click(function(){
+      $('body').toggleClass('visible_menu');
+    })  
 });
