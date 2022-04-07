bbd778c8da024e313c66c3dd0f2415d79c103086
Ezequiel Rico <ezequielrico@outlook.com>
2022-04-07 01:48:34 -0300

    entrega 2

--- functionss.js
+++ functionss.js
@@ -1,1 +1,7 @@
-alert ('Selecciona un producto del site y tipeelo en la consola');
+let tarjeta = parseFloat(prompt("Ingrese los 16 números de su tarjeta:"));
+let vencimiento = parseFloat(prompt("Ingrese mes y año del vencimiento de su tarjeta: MM/AA"));
+let cod = parseFloat(prompt("Ingrese el código de seguridad de su tarjeta:"));
+alert(`¿Están bien los siguientes datos?: ${tarjeta}
+${vencimiento}
+${cod}
+Si está bien, pulse aceptar, de lo contrario, recargue la página.`);
