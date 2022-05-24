000e6b83c452ab9a04a3560b5781db9983a44441
Ezequiel Rico <ezequielrico@outlook.com>
2022-05-23 23:51:16 -0300

    probando el menu desplegable

--- functions.js
+++ functions.js
@@ -1,3 +1,20 @@
+// const menux = document.querySelector('#hamburguesa');
+// const menu_desplegado = document.querySelector('nav');
+// const menu_oculto = document.querySelector('#cerrarmenux');
+
+// menux.addEventListener("click",()=>{
+//   menu_desplegado.classList.add("open");
+// })
+
+// menuxclose.addEventListener("click",()=>{
+//   menu_desplegado.classList.remove("open");
+// })
+
+// menu_desplegado.addEventListener("click",(e)=>{
+//   e.target.classList.contains("carrito") && carritoAbierto.classList.remove("open")
+// })
+
+
 $( document ).ready(function() {
     $('#menu_on').click(function(){
     	$('body').toggleClass('visible_menu');
