883e668c95bcd241b4b99247dd5692513f76e42f
EZE_ <90348044+EZEweb@users.noreply.github.com>
2025-09-03 15:56:43 -0300

    Delete functions.js

--- functions.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    $('#menu_on').click(function(){
-    	$('body').toggleClass('visible_menu');
-    })
-    $('#cerrarmenux').click(function(){
-      $('body').toggleClass('visible_menu');
-    })  
-});
